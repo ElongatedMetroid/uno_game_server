66f34b9c9eedceeab15a4c9f11b89cf375e4dc0e
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-09-04 14:48:45 -0700

    switch to using lib_uno_game so the server and client can share

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::{net::{TcpListener, TcpStream}, process, sync::{Arc, Mutex}};
 
 mod threadpool;
-mod game;
 
+use lib_uno_game::Game;
 use threadpool::ThreadPool;
-use game::Game;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap_or_else( |error| {
@@ -18,18 +17,33 @@ fn main() {
     let game = Arc::new(Mutex::new(game));
 
     for stream in listener.incoming() {
+        let mut turn = 0;
         let game = Arc::clone(&game);
 
-        pool.execute(|| {
-            handle_client(stream.unwrap(), game);
+        pool.execute(move || {
+            handle_client(stream.unwrap(), game, turn);
         });
+
+        turn += 1;
     }
 }
 
-fn handle_client(stream: TcpStream, game: Arc<Mutex<Game>>) {
+fn handle_client(stream: TcpStream, game: Arc<Mutex<Game>>, turn: usize) {
+    // Recieve initial data, like name
+
+
+    player.set_turn(turn);
+
     // game.add_player(recieved.player);
 
+    // When all players are ready send cards to each
+
     loop {
-        // Clone Arc<Mutex<Game>> in a Game, and send the Game structure to the client
+        // Clone Arc<Mutex<Game>> in a Game, and send the Game structure to the client-
+
+        // Recieve card
+        // (check if its valid)
+
+        // Next player turn
     }
 }
